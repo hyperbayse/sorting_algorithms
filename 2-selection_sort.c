b9b152790847d71362c5228c4d47790ede93f5f5
hyperbayse <hyperbayse@gmail.com>
2024-11-03 21:22:54 +0100

    update

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,5 +8,21 @@
  */
 void selection_sort(int *array, size_t size)
 {
+	size_t i, j;
 	int x;
+
+	for (i = 0; i < size; i++)
+	{
+		x = array[i];
+		for (j = i; j < size; j++)
+		{
+			if (x > array[j])
+			{
+				x = array[j];
+				array[j] = array[i];
+				array[i] = x;
+				print_array(array, size);
+			}
+		}
+	}
 }
